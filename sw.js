efb205e1d234c6b4ec135b41314b96e6ce50c952
DaviBequer <davijr123.bequer@gmail.com>
2026-08-01 23:12:20 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'siloe-cache-v12';
+const ARQUIVOS = [
+  './siloe.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ARQUIVOS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((nomes) =>
+      Promise.all(
+        nomes.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((resp) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, resp.clone());
+          return resp;
+        });
+      }).catch(() => cached);
+    })
+  );
+});
